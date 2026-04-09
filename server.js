569e7a41eef718135e0d5f4006eb0ef6093ec8cb
Jannis <jannis@example.com>
2026-04-09 14:08:40 +0200

    fix: correct HTML file paths for Heroku

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'HTML', 'Homepage.html'));
 });
 
+app.get('/HTML/Dashboard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML', 'Dashboard.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server läuft auf Port ${PORT}`);
 });
