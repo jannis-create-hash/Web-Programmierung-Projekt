ee58df18c98aa34a5b835f9e81dedcda1c856b74
Jannis <jannis@example.com>
2026-04-09 14:21:29 +0200

    fix: update backend URL to dedicated API server

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Backend-URL hier eintragen sobald Heroku-App erstellt wurde
-const BACKEND_URL = 'https://teamgeist-hr-eb0b3ba81e40.herokuapp.com';
+const BACKEND_URL = 'https://teamgeist-hr-api-d9923c06a4fa.herokuapp.com';
