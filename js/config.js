024163f76e7c0368c6993c80e77012125b14a422
Jannis <jannis@example.com>
2026-04-09 14:05:41 +0200

    feat: add express server for MPA

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Backend-URL hier eintragen sobald Heroku-App erstellt wurde
-const BACKEND_URL = 'https://teamgeist-hr-backend.herokuapp.com';
+const BACKEND_URL = 'https://teamgeist-hr-eb0b3ba81e40.herokuapp.com';
